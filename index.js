5f6121c74c5bb3d3196879fccbd4565d324fc8ca
dev-mahfuz <mrsohag04@gmail.com>
2023-01-19 19:04:45 +0600

    create-update-workspace function added

--- index.js
+++ index.js
@@ -27,16 +27,41 @@ function verifyJWT(req, res, next) {
     })
 }
 
+
 const uri = `mongodb+srv://${process.env.DBV_USER}:${process.env.DB_PASSWORD}@cluster0.yzlpmea.mongodb.net/?retryWrites=true&w=majority`;
 
 async function run() {
     try {
         const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
         const userCollection = client.db('taskMaster').collection('users');
+        const workspaceCollection = client.db('taskMaster').collection('workspaces');
+
+        app.post('/create-update-workspace', verifyJWT, async (req, res) => {
+            const decoded = req.decoded;
+            const s = req.body;
+            let result;
+            if (s._id == 'new') {
+                delete s._id;
+                const day = new Date(Date.now());
+                s.created = day;
+                s.users = [
+                    { uid: decoded._id, date: day, role: 'admin', invited: false }
+                ]
+                result = await workspaceCollection.insertOne(s);
+            } else {
+                const query = { _id: ObjectId(s._id) }
+                delete s._id;
+                const updatedDoc = {
+                    $set: s
+                }
+                result = await workspaceCollection.updateOne(query, updatedDoc);
+
+            }
+            res.send(result);
+        });
 
         app.post('/jwtANDusers', async (req, res) => {
             const u = req.body;
-
             const query = { email: u.email };
             let user = await userCollection.findOne(query);
             if (!user && u?.insert) {
@@ -45,7 +70,7 @@ async function run() {
                 user = await userCollection.findOne(query);
             }
             if (user) {
-                let token = jwt.sign({ email: u.email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' });
+                let token = jwt.sign({ email: u.email, _id: u._id }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' });
                 let role = user.role;
                 return res.send({ token, role });
             }
